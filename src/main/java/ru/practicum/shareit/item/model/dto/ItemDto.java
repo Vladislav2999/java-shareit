1fa575e6ee31586cae40ad469845d46866cf18cd
Владислав <v.konykhov@yandex.ru>
2023-02-14 21:39:03 +0300

    загрузка проекта

---
+++ src/main/java/ru/practicum/shareit/item/model/dto/ItemDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.item.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto {
+
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private Long requestId;
+}

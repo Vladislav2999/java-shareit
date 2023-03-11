afa5ab34412d70d74eeba131ba258ff143912787
Владислав <v.konykhov@yandex.ru>
2023-03-12 00:26:34 +0300

    Удаление ненужного пакета

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.item.model.Item;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
